0a556aa91bbe2212a7ffd2b73ade7d069870a8dc
markor33 <markorpc1@gmail.com>
2024-01-30 16:14:05 +0100

    Implement file storing to minio

--- server-side/src/main/java/com/example/udd/service/interfaces/LawService.java
+++ server-side/src/main/java/com/example/udd/service/interfaces/LawService.java
@@ -1,7 +1,9 @@
 package com.example.udd.service.interfaces;
 
+import io.minio.GetObjectResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface LawService {
     String indexDocument(final MultipartFile documentFile);
+    GetObjectResponse loadAsResource(final String serverFilename);
 }
